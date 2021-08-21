6893230bbd2ab153fe1f2d37abac1271159eefef
chimpaji <chimpaji@outlook.com>
2021-08-21 19:23:51 +0700

    init git

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [people, setPeople] = useState([
+    { name: "Lebron James", url: "", age: 20, notes: "Hi" },
+    { name: "Kobe Bryant", url: "", age: 36 },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to my party</h1>
     </div>
   );
 }
